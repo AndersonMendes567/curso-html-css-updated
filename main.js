5ed1bf7299a4f1e7f62fe66b0b79bfe960a3bc96
Anderson Mendes <mendesanderson567@gmail.com>
2021-02-16 22:32:44 -0300

    inicial

---
+++ main.js
@@ -0,0 +1,6 @@
+var name = "Anderson";
+console.log("Seu nome é " + name);
+var nascDate = 2000;
+var atualDate = 2020;
+var age = (atualDate - nascDate);
+console.log(name + " tem " + age + " anos!");
